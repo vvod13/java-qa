2b55f2fa297ab5054bb22bb0a7e73f82e04dada5
Mike <levinmk23@gmail.com>
2023-06-29 10:27:45 +0300

    Lesson 4. Массивы

---
+++ src/main/java/ru/yandex/lesson4/HomeWork.java
@@ -0,0 +1,48 @@
+package ru.yandex.lesson4;
+
+public class HomeWork {
+
+
+    /**
+     * Функция должна проверять является ли переданный массив
+     * отсортированным.
+     * <p>
+     * 1 2 3 3 4 5 -> true
+     * 1 2 1 5 -> false
+     * 5 4 1 -> true
+     * */
+    public static boolean isSorted(int[] array) {
+        return false;
+    }
+
+    /**
+     * Функция должна возвращать максимальное произведение
+     * двух элементов массива
+     * Массив может состоять как из положительных, так и из
+     * отрицательных чисел
+     * 1 5 3 1 4 -> 4 * 5 = 20
+     * */
+    public static long maxTwinMultiply(int[] array) {
+        return 0;
+    }
+
+    // Две задачи ниже по желанию. Но они интересные
+    /**
+     * aaaabbbcdd -> 4(a)3(b)1(c)2(d)
+     * abc -> 1(a)1(b)1(c)
+     * aaaaaaaaaa -> 10(a)
+     * */
+    public static String rleCompress(String src) {
+        return "";
+    }
+
+    /**
+     * 4(a)3(b)1(c)2(d) -> aaaabbbcdd
+     * 1(a)1(b)1(c) -> abc
+     * 10(a) -> aaaaaaaaaa
+     * */
+    public static String rleDecompress(String src) {
+        return "";
+    }
+
+}
